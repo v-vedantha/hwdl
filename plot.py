9d2e0274a07d1c69c258b82bc820681c8d596295
Vedantha Venkatapathy <vedantha@Vedanthas-MacBook-Pro.local>
2024-05-01 01:41:40 -0400

    Added error bars

--- plot.py
+++ plot.py
@@ -1,21 +1,34 @@
 import matplotlib.pyplot as plt
 from test import *
+import math
+import numpy
+class Trial:
+    def __init__(self):
+        self.vals = []
+    def add(self, val):
+        self.vals.append(val)
+    def mean(self):
+        return sum(self.vals) / len(self.vals)
+    def std(self):
+        return numpy.std(self.vals)
 
-
+trials = 10
 # Plot 1. N = T. 2 streams of the same length, but increasing density.
 densities = [0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5]
-results=[[], [], [], []]
-l = 1000
+results=[[],[],[],[]]
 for d in densities:
-    temp = test_matmul(d, d, 1, l, 1, 10, 10, 10)
-    print(temp)
-    assert(len(temp)   == 4)
     for i in range(4):
-        results[i].append(temp[i][1])
-
-print(results)
+        results[i].append(Trial())
+l = 1000
+for trial in range(trials):
+    for idx, d in enumerate(densities):
+        temp = test_matmul(d, d, 1, l, 1, 10, 10, 10)
+        print(temp)
+        assert(len(temp)   == 4)
+        for i in range(4):
+            results[i][idx].add(temp[i][1])
 for i in range(4):
-    plt.plot(densities, results[i], label=collider_names[i])
+    plt.errorbar(densities, list(map(lambda x : x.mean(), results[i])), list(map(lambda x: x.std(), results[i])), label=collider_names[i])
 plt.xlabel('Density')
 plt.ylabel('Cycles')
 plt.title('Density vs Cycles for a 1000 length array')
@@ -32,15 +45,18 @@ lengths = [100, 200, 300, 400, 500, 600, 700]
 results=[[], [], [], []]
 density = 0.05
 for l in lengths:
-    temp = test_matmul(density, density, 1, l, 1, 10, 10, 10)
-    print(temp)
-    assert(len(temp)  == 4)
     for i in range(4):
-        results[i].append(temp[i][1])
+        results[i].append(Trial())
+for trial in range(trials):
+    for idx, l in enumerate(lengths):
+        temp = test_matmul(density, density, 1, l, 1, 10, 10, 10)
+        print(temp)
+        assert(len(temp)  == 4)
+        for i in range(4):
+            results[i][idx].add(temp[i][1])
 
-print(results)
 for i in range(4):
-    plt.plot(lengths, results[i], label=collider_names[i])
+    plt.errorbar(lengths, list(map(lambda x : x.mean(), results[i])), list(map(lambda x: x.std(), results[i])), label=collider_names[i])
 plt.xlabel('Density')
 plt.ylabel('Cycles')
 plt.title('Array length vs Cycles for a 0.05 dense length array')
@@ -55,15 +71,18 @@ results=[[], [], [], []]
 density = 0.05
 length = 1000
 for t in Ts:
-    temp = test_matmul(density, density, 1, l, 1, t, t, t)
-    print(temp)
-    assert(len(temp)  == 4)
     for i in range(4):
-        results[i].append(temp[i][1])
+        results[i].append(Trial())
+for trial in range(trials):
+    for idx, t in enumerate(Ts):
+        temp = test_matmul(density, density, 1, l, 1, t, t, t)
+        print(temp)
+        assert(len(temp)  == 4)
+        for i in range(4):
+            results[i][idx].add(temp[i][1])
 
-print(results)
 for i in range(4):
-    plt.plot(Ts, results[i], label=collider_names[i])
+    plt.errorbar(Ts, list(map(lambda x : x.mean(), results[i])), list(map(lambda x: x.std(), results[i])), label=collider_names[i])
 plt.xlabel('T/N')
 plt.ylabel('Cycles')
 plt.title('T/N vs Cycles for a 0.05 dense 1000 length array')
@@ -73,16 +92,49 @@ plt.savefig('plt3.png')
 plt.clf()
 
 # Plot skips for ideal and hardware version
-results = test_skips(0.05, 0.05, 1, 1000, 1, 10, 10, 10)
+# results_hist = []
+# for trial in range(trials):
+#     results_hist.append(test_skips(0.05, 0.05, 1, 1000, 1, 10, 10, 10))
+
+# results_lookahead = []
+# results_friendly = []
+# for result in results_hist:
+#     results_lookahead.append(result[0])
+#     results_friendly.append(result[1])
+
+# hists = []
+# for result in results_lookahead:
+#     hists.append(numpy.histogram(result[1], bins=range(10))[0])
+# hists=numpy.vstack(hists)
+# breakpoint()
+# stds = numpy.std(hists, axis=1)
+# means = numpy.mean(hists, axis=1)
+# plt.bar(range(10), means, label=collider_names[result[0]], alpha=0.5, yerr=stds)
+
+# hists = []
+# for result in results_friendly:
+#     hists.append(numpy.histogram(result[1], bins=range(10))[0])
+# hists=numpy.vstack(hists)
+# stds = numpy.std(hists, axis=1)
+# means = numpy.mean(hists, axis=1)
+# plt.bar(range(10), means, label=collider_names[result[0]], alpha=0.5, yerr=stds)
+# plt.xlabel('Skips')
+# plt.ylabel('Frequency')
+# plt.title('Skips for 0.05 dense 1000 length array')
+# plt.legend()
+
+# plt.savefig("plt4.png")
+# plt.clf()
 
 # Plot a histogram of the results
+results = test_skips(0.05, 0.05, 1, 1000, 1, 10, 10, 10)
 for result in results:
     plt.hist(result[1], bins=8, label=collider_names[result[0]], alpha=0.5)
 plt.xlabel('Skips')
 plt.ylabel('Frequency')
 plt.title('Skips for 0.05 dense 1000 length array')
 plt.legend()
-plt.savefig('plt4.png')
+plt.savefig('plt5.png')
 
 
 
